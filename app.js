04267eace6b1c88bbd2bf319255f3aff40259a69
Onur Kaplan <onur.kaplan@mynetgroup.com>
2020-05-26 02:00:38 +0300

    remove item integrated is done

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
-let name = document.getElementById('name');
-let surName = document.getElementById('surName');
-let phone = document.getElementById('phone');
+const name = document.getElementById('name');
+const surName = document.getElementById('surName');
+const phone = document.getElementById('phone');
 const list = document.getElementById('person-list');
 
 class DBManager {
-
     getItem(){
         let people;
         if(localStorage.getItem('people')===null){
@@ -17,21 +16,39 @@ class DBManager {
     setItem(person){
         let people = this.getItem();
         people.push(person);
-        localStorage.setItem('people',JSON.stringify(people));
+        localStorage.setItem('people', JSON.stringify(people));
+    }
+    findItem(person){
+        if (this.getItem().some(item => item.phone === person)) {
+            return true;
+        }
+    }
+    updateItem(data){
+        localStorage.setItem('people', JSON.stringify(data));
+    }
+    removeItem(id){
+        let addedPerson = this.getItem()
+        let position = addedPerson.map(e => { 
+            return e.phone; }
+            ).indexOf(id);
+        addedPerson.splice(position,1);
+        this.updateItem(addedPerson);
     }
 
 }
 
 function addPersonToList(person) {
     var html = `
-     <tr>
+     <tr id="${person.phone}">
         <td>${person.name}</td>
         <td>${person.surName}</td>
         <td>${person.phone}</td>
+        <td class="remove-btn">Sil</td>
      </tr>    
 `;
     list.innerHTML += html;
 }
+
 
 
 document.querySelector(".add-person-btn").addEventListener('click',function(){
@@ -42,12 +59,12 @@ document.querySelector(".add-person-btn").addEventListener('click',function(){
     }
 
     let db = new DBManager();
-    let currentDb = db.getItem();
+
     if(name.value==='' || surName.value ==='' || phone.value === ''){
         alert("Eksik alan");
         return;
     }
-    if (currentDb.some(person => person.phone === phone.value)) {
+    if(db.findItem(phone.value)){
         alert("Bu kişi zaten Var");
         return;
     }
@@ -60,6 +77,16 @@ document.querySelector(".add-person-btn").addEventListener('click',function(){
     phone.value= '';
 });
 
+
+list.addEventListener("click", function(e){
+    let targetElm = e.target;
+    let itemId = targetElm.parentElement.getAttribute("id");
+    let db = new DBManager();
+    if(targetElm.classList.contains("remove-btn")){
+      targetElm.parentElement.remove();
+      db.removeItem(itemId)
+    }
+})
 
 function init(){
     let db = new DBManager();
